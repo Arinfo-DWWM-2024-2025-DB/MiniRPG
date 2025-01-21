9eb448972285427310635c1781dca0e69687d719
JasonSaluden <saluden.jason@gmail.com>
2025-01-21 13:41:55 +0100

    css added

--- hero.php
+++ hero.php
@@ -50,7 +50,8 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Création d'un personnage</title>
-<link rel="stylesheet" href="css/hero.css"
+<link rel="stylesheet" href="/css/hero.css">
+
 </head>
 
 <body>
