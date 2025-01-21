582f5f88f1d025ed114ec155365741a530ab73e5
Tana PICH <raismey.tana@gmail.com>
2025-01-21 16:05:15 +0100

    update

--- combat.php
+++ combat.php
@@ -25,3 +25,21 @@ while ($_SESSION['hpHero'] > 0 && $_SESSION['hpMonstre'] > 0) {
     $_SESSION['compteur']++;
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/combat.css">
+    <title>Mini RPG - Combat</title>
+</head>
+
+<body>
+    <form method="POST">        
+        <button type="submit" name="reset">Reset</button>
+    </form>
+</body>
+
+</html>
